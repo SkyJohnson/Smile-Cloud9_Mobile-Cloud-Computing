627c9cfdd19ebbc37b03dbe94f4a3fb3ec73f9f8
Jomengan <44821720+Jomengan@users.noreply.github.com>
2020-04-27 18:50:51 -0700

    Add files via upload

---
+++ Receiver.py
@@ -0,0 +1,72 @@
+import time 
+import serial
+import sys
+import Jetson.GPIO as GPIO
+import numpy as np
+ser = serial.Serial(
+	port = '/dev/ttyTHS1',
+	baudrate = 115200,
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout = 0
+	)
+counter = 1
+vect = bytearray()
+#Zero_Matrix = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Zero_Matrix = list(np.zeros((144), int))
+
+#Matrix = []
+matrixempty = 1
+narray = []
+time_start = time.time()
+while 1:
+		time_start1 = time.time()
+	while ser.inWaiting():
+		vect= ser.read(144)
+		time_start2 = time.time()
+		rec = time_start2 - time_start1
+		print('Recieve time:', (rec))
+		if vect == b'Finish':
+			time_end = time.time()
+			time_total = time_end - time_start
+			print(time_total)
+			sys.exit()
+		print(vect)
+		rdata=list(vect)
+		print(rdata)
+		
+		print(len(rdata))
+		if len(rdata) < len(Zero_Matrix):
+			rarray = Zero_Matrix
+		else:
+			rarray = np.array([rdata])
+		
+		print(rarray) 
+		if matrixempty:
+			Matrix = rarray
+			matrixempty = 0
+			print('Matrix')
+			print(Matrix)
+		else:
+			#Matrix = np.concatenate((Matrix, rarray), axis = 0)
+			Matrix = np.vstack((Matrix, rarray))
+			#if len(Matrix)			
+			print('Matrix')
+			print(Matrix)
+
+		#rdata.clear()
+
+		#x = ser.readline()
+		#if x == '1':
+			#print('Message Recieved %s\n'%(x))
+			#y = int(x) + 1
+			#print('New Value: %d' %(y))
+			#newValue = '\nNew Value: %d\n' %(y)
+			#ser.write(str.encode(newValue))
+			#time.sleep(1)
+		#else:
+			#print(x)
+			#reply = 'Recieved'
+			#ser.write(str.encode(reply))
+			#time.sleep(1)
---
+++ Transmitter.py
@@ -0,0 +1,103 @@
+import time
+import serial
+import random
+import RPi.GPIO as GPIO
+import numpy as np
+
+ser = serial.Serial(
+    port = '/dev/ttyS0',
+    baudrate = 115200,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 0
+    )
+counter = 0
+sent = 0
+recieved = 1
+
+#pic = np.random.randint(0,255, size=(20,20), dtype=np.uint8)
+pic = np.random.randint(0,255, size=(256,144), dtype=np.uint8)
+#pic = np.random.randint(0,255, size=(480,360))
+#pic = np.random.randint(0,255, size=(720,480))
+#pic = np.random.randint(0,255, size=(1280,720))
+#pic = np.random.randint(0,255, size=(1280,720))
+num_rows = 144
+num_columns = 480
+
+#pic = [[random.randint(0,255) for j in range(0, num_rows)] for i in range(num_columns)]
+
+time_start = time.time()
+#print(pic)
+
+for row in pic:
+    #list_row = list(row)
+    data = bytearray(row)
+    print(row)
+    print(data)
+    print(len(data))
+    #ser.write(str.encode(row))
+    ser.write(data)
+    time.sleep(5)
+ser.write(str.encode('Finish'))
+    
+time_end = time.time()
+
+time_tot= time_end - time_start
+
+print(time_tot)
+
+#while 1:
+   # ser.write(str.encode('Write counter: %d\n'%(counter)))
+    #time.sleep(1)
+    #counter += 1
+    
+#     ser.write(str.encode('1'))
+#     x = ser.readline().decode()
+#     print(x)
+#     time.sleep(2)
+#         
+#     ser.write(str.encode('6'))
+#     x = ser.readline().decode()
+#     print(x)
+#     time.sleep(2)
+
+
+
+# 
+#     data2 = bytearray(elements)
+#     
+#     ser.write(data2)
+#     print(data2)
+#     time.sleep(2)
+#     
+#     data2 = bytearray(elements1)
+#     
+#     ser.write(data2)
+#     print(data2)
+#     time.sleep(2)
+#     
+#     data2 = bytearray(elements2)
+#     
+#     ser.write(data2)
+#     print(data2)
+#     time.sleep(2)
+
+
+
+#     if x == '1':
+#         print('Message Recieved %s\n'%(x))
+#         y = int(x) + 1
+#         print('New Value: %d' %(y))
+#         ser.write(str.encode('\n New Value %d\n'%(y)))
+#         time.sleep(3)
+#     else:
+#         print('Message Recieved %s\n'%(x))
+#         check = x.isdigit()
+#         if check:
+#             y = int(x) + 20
+#             print('New Value: %d\n'%(y))
+#             ser.write(str.encode('\n New Value %d\n'%(y)))
+#         else:
+#             ser.write(str.encode('\n Please send a number\n'))
+#             print('Waiting Int')  
