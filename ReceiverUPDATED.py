895e2a49613b99134472e8cab3b1f23e2d0eef80
Jomengan <44821720+Jomengan@users.noreply.github.com>
2020-05-04 12:31:39 -0700

    Add files via upload

---
+++ ReceiverUPDATED.py
@@ -0,0 +1,126 @@
+import time 
+import serial
+import sys
+import Jetson.GPIO as GPIO
+import numpy as np
+
+ser = serial.Serial(
+	port = '/dev/ttyTHS1',
+	baudrate = 115200,
+	parity = serial.PARITY_NONE,
+	stopbits = serial.STOPBITS_ONE,
+	bytesize = serial.EIGHTBITS,
+	timeout = .5
+	)
+#counter = 1
+#vect = bytearray()
+#Zero_Matrix = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Zero_Matrix = list(np.zeros((27), int))
+
+Matrix = []
+matrixempty = 1
+narray = []
+zero_count = 0
+time_start = time.time()
+
+
+def receiver():
+	global matrixempty, Matrix, narray, zero_count, time_start
+	while ser.inWaiting() > 0:
+		vect = ser.read(size=27)
+		if vect == b'Image':
+			image()
+			break
+		if vect == b'Finish':
+			finish()
+		print(vect)
+		rdata=list(vect)
+		print(rdata)
+		
+		print(len(rdata))
+		if len(rdata) < len(Zero_Matrix):
+			rarray = Zero_Matrix
+			zero_count = zero_count + 1
+		else:
+			rarray = np.array([rdata])
+		
+		print(rarray) 
+		if matrixempty:
+			Matrix = rarray
+			matrixempty = 0
+			print('Matrix')
+			print(Matrix)
+		else:
+			#Matrix = np.concatenate((Matrix, rarray), axis = 0)
+			Matrix = np.vstack((Matrix, rarray))
+			#if len(Matrix)			
+			print('Matrix')
+			print(Matrix)
+
+
+def image():
+	global matrixempty, Matrix, narray, zero_count, time_start
+	datafile = open('data.txt', 'a')
+	datafile.write(str(Matrix))
+	datafile.write('\n')
+	datafile.write('\n')
+	datafile.close()
+	Matrix = []
+	matrixempty = 1
+	receiver()
+
+def finish():
+	global matrixempty, Matrix, narray, zero_count, time_start
+	time_end = time.time()
+	time_total = time_end - time_start
+	correct_lines = 256-zero_count
+	accuracy = correct_lines/256
+	#print('Accuracy:',accuracy)
+	print('Total Time:', time_total)
+	sys.exit()
+
+while 1:
+	#time_start1 = time.time()
+	receiver()
+	#while ser.inWaiting() > 0:
+	#	vect = ser.read(size=128)
+	#	time_start2 = time.time()
+	#	rec = time_start2 - time_start1
+	#	#print('Recieve time:', (rec))
+	#	if vect == b'Image':
+	#		datafile = open('data.txt', 'a')
+	#		datafile.write(str(Matrix))
+	#		Matrix = []
+	#		matrixempty = 1
+	#	if vect == b'Finish':
+	#		time_end = time.time()
+	#		time_total = time_end - time_start
+	#		correct_lines = 256-zero_count
+	#		accuracy = correct_lines/256
+	#		#print('Accuracy:',accuracy)
+	#		print('Total Time:', time_total)
+	#		sys.exit()
+	#	print(vect)
+	#	rdata=list(vect)
+	#	print(rdata)
+	#	
+	#	print(len(rdata))
+	##		rarray = Zero_Matrix
+	#		zero_count = zero_count + 1
+	#	else:
+	#		rarray = np.array([rdata])
+	#	
+	#	print(rarray) 
+	#	if matrixempty:
+	#		Matrix = rarray
+	#		matrixempty = 0
+	#		print('Matrix')
+	#		print(Matrix)
+	#	else:
+	#		#Matrix = np.concatenate((Matrix, rarray), axis = 0)#
+	#		Matrix = np.vstack((Matrix, rarray))
+	#		#if len(Matrix)			
+	#		print('Matrix')
+	#		print(Matrix)
+
+
