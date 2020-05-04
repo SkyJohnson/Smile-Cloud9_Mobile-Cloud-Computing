2917afd8c47ff89ba8597043b2d7c082237dbe1a
Jomengan <44821720+Jomengan@users.noreply.github.com>
2020-05-04 12:38:13 -0700

    Update TransmitterUPDATED.py

--- TransmitterUPDATED.py
+++ TransmitterUPDATED.py
@@ -12,97 +12,51 @@ ser = serial.Serial(
     bytesize = serial.EIGHTBITS,
     timeout = 20
     )
-counter = 0
-sent = 0
-recieved = 1
 
+
+#DIFFERENT SIZES OF MATRICES FOR TESTING
+#Main two dimensions used for the project
 pic = np.random.randint(0,255, size=(27,27), dtype=np.uint8)
 #pic = np.random.randint(0,255, size=(128,128), dtype=np.uint8)
+
+#Test dimensions for comparisson 
 #pic = np.random.randint(0,255, size=(256,144), dtype=np.uint8)
 #pic = np.random.randint(0,255, size=(480,360), dtype=np.uint8)
-#pic = np.random.randint(0,255, size=(720,480), dtype=np.uint8)
-#pic = np.random.randint(0,255, size=(1280,720), dtype=np.uint8)
-#pic = np.random.randint(0,255, size=(1280,720))
 
 
-#pic = [[random.randint(0,255) for j in range(0, num_rows)] for i in range(num_columns)]
+
+
 
 time_start = time.time()
 #print(pic)
+
+#TO SEND MULTIPLE MATRICES BACK TO BACK INCREASE i 
 i = 0
 
 while i < 1:
+    #SEND EACH MATRIX BY READING EACH ROW IN THE MATRIX AND TRASNMIST IT SERIALLY
     for row in pic:
         #list_row = list(row)
         data = bytearray(row)
         print(row)
         print(data)
         print(len(data))
-    #ser.write(str.encode(row))
         ser.write(data)
         time.sleep(.1)
+    #Once the whole matrix is transmitted, tell the receiver that the image is done
+    #The transmitted will know that the matrix is finished and that the next message will either be a 'Finish' message which stops receiving
+    #or the next matrix is about to be sent
     ser.write(str.encode('Image'))
     time.sleep(1)
     i = i + 1
+   
+
+#Once all matrices are transmitted, tell the receiver that it is finished and no more matrices are to be transmitted
 ser.write(str.encode('Finish'))
-    
+
+#Record timings of transmission/execution time
 time_end = time.time()
-
 time_tot= time_end - time_start
-
 print(time_tot)
 
-#while 1:
-   # ser.write(str.encode('Write counter: %d\n'%(counter)))
-    #time.sleep(1)
-    #counter += 1
-    
-#     ser.write(str.encode('1'))
-#     x = ser.readline().decode()
-#     print(x)
-#     time.sleep(2)
-#         
-#     ser.write(str.encode('6'))
-#     x = ser.readline().decode()
-#     print(x)
-#     time.sleep(2)
 
-
-
-# 
-#     data2 = bytearray(elements)
-#     
-#     ser.write(data2)
-#     print(data2)
-#     time.sleep(2)
-#     
-#     data2 = bytearray(elements1)
-#     
-#     ser.write(data2)
-#     print(data2)
-#     time.sleep(2)
-#     
-#     data2 = bytearray(elements2)
-#     
-#     ser.write(data2)
-#     print(data2)
-#     time.sleep(2)
-
-
-
-#     if x == '1':
-#         print('Message Recieved %s\n'%(x))
-#         y = int(x) + 1
-#         print('New Value: %d' %(y))
-#         ser.write(str.encode('\n New Value %d\n'%(y)))
-#         time.sleep(3)
-#     else:
-#         print('Message Recieved %s\n'%(x))
-#         check = x.isdigit()
-#         if check:
-#             y = int(x) + 20
-#             print('New Value: %d\n'%(y))
-#             ser.write(str.encode('\n New Value %d\n'%(y)))
-#         else:
-#             ser.write(str.encode('\n Please send a number\n'))
-#             print('Waiting Int')  
